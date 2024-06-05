635b9e62d8440e21e7a409a2fecf6ea3f2e91a3d
Pablo Portilla Rivero <pportilla@rfe.es>
2024-06-05 15:48:34 +0200

    Se añade los endpoint de get por el Id y el de actualizar por el id tambien.

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,10 +1,12 @@
 const express= require('express');
 const router=express.Router();
 
+const mongoose = require('mongoose');
+
+
 const Producto=require('../models/Product.js');
 
 router.post('/create', async (req, res)=>{
-    console.log(Object.keys(req.body));
     try{
         if(Object.keys(req.body).length===0){
             res.status(400).send('Error al introducir el objeto: Esta vacío');
@@ -30,6 +32,32 @@ router.get('/read', async (req, res)=>{
     }
     catch{
         res.status(401).send('Error al leer la base de datos');
+    }
+})
+
+//Utilizaremos el ID que asigna la base de datos como el tipico nº de referencia que tienen los productos
+router.get('/read/:id', async(req, res)=>{
+    try{
+        const {id}=req.params;
+        const producto=await Producto.findById(id);
+        res.send(producto);
+    }
+    catch{
+        res.status(400).send('Error: No hay prenda con este numero de referencia');
+    }
+})
+
+router.put('/update/:id', async(req, res)=>{
+    try{
+        if(Object.keys(req.body).length===0){
+            res.status(400).send('Error al actualizar el objeto: Esta vacío');
+        }
+        const {id}=req.params;
+        const producto=await Producto.findByIdAndUpdate(id, req.body);
+        res.send(producto);
+    }
+    catch{
+        res.status(400).send('Error: No se ha podido actualizar la prenda');
     }
 })
 
