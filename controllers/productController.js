5b1bef860bee6319b78c6556b36e5131b34e033b
Pablo Portilla Rivero <pportilla@rfe.es>
2024-06-09 20:21:53 +0200

    Se cambia la renderizacion utilizando pug y ademas en varios endpoints se lleva todo el funcionamiento al controller.

--- controllers/productController.js
+++ controllers/productController.js
@@ -12,72 +12,65 @@ async function createProduct(producto){
     }
 }
 
-async function showProducts(opt){
+async function showProducts(req, res){
     try{
+        var Enlace=false;
+        if(req.path==='/products'){
+            Enlace=true;
+        }
         const productos=await Producto.find();
-        const html_arr=[];
-        if(opt===1){
-            productos.forEach(producto=>html_arr.push(generarProductoHTMLConEnlace(producto)))
-        }
-        else{
-            productos.forEach(producto=>html_arr.push(generarProductoHTML(producto)))
-        }
-        const html=html_arr.join('')
-        console.log(html)
-        return html;
+        res.render('objeto', {productos, conEnlace:Enlace})
     }catch (error) {
         console.error('Error al mostrar el producto:', error);
         throw new Error('Error al mostrar el producto');
     }
 }
 
-async function showProductById(id){
+async function showProductById(req ,res){
     try{
+        const id=req.params.id
         const producto=await Producto.findById(id);
-        console.log(producto);
-        const html=generarProductoHTML(producto);
-        console.log(html);
-        return html;
+        res.render('objeto', {producto, conEnlace:false})
     }catch (error) {
         console.error('Error al mostrar el producto:', error);
         throw new Error('Error al mostrar el producto');
     }
 }
 
-function generarProductoHTML(producto) {
+// function generarProductoHTML(producto) {
     
-    return `
-        <html>
-        <body>
-            <p><strong>Referencia:</strong> ${producto._id}</p>
-            <p><strong>Descripción:</strong> ${producto.descripcion}</p>
-            <p><strong>Categoría:</strong> ${producto.categoria}</p>
-            <p><strong>Precio:</strong> ${producto.precio}</p>
-            <p><strong>Tamaño:</strong> ${producto.talla}</p>
-            <img src="${producto.imagen}" alt="${producto.nombre}">
+//     return `
+//         <html>
+//         <body>
+//             <p><strong>Referencia:</strong> ${producto._id}</p>
+//             <p><strong>Descripción:</strong> ${producto.descripcion}</p>
+//             <p><strong>Categoría:</strong> ${producto.categoria}</p>
+//             <p><strong>Precio:</strong> ${producto.precio}</p>
+//             <p><strong>Tamaño:</strong> ${producto.talla}</p>
+//             <img src="${producto.imagen}" alt="${producto.nombre}">
 
-        </body>
-        </html>
-    `;
-}
+//         </body>
+//         </html>
+//     `;
+// }
 
 
-function generarProductoHTMLConEnlace(producto) {
-    return `
-        <html>
-        <body>
-            <p><strong>Referencia:</strong> ${producto._id}</p>
-            <p><strong>Descripción:</strong> ${producto.descripcion}</p>
-            <p><strong>Categoría:</strong> ${producto.categoria}</p>
-            <p><strong>Precio:</strong> ${producto.precio}</p>
-            <p><strong>Tamaño:</strong> ${producto.talla}</p>
-            <a href="/products/${producto._id}">
-                <img src="${producto.imagen}" alt="${producto.nombre}">
-            </a>
-        </body>
-        </html>
-    `;
-}
+// function generarProductoHTMLConEnlace(producto) {
+//     return `
+//         <html>
+//         <body>
+//             <p><strong>Referencia:</strong> ${producto._id}</p>
+//             <p><strong>Descripción:</strong> ${producto.descripcion}</p>
+//             <p><strong>Categoría:</strong> ${producto.categoria}</p>
+//             <p><strong>Precio:</strong> ${producto.precio}</p>
+//             <p><strong>Tamaño:</strong> ${producto.talla}</p>
+//             <a href="/products/${producto._id}">
+//                 <img src="${producto.imagen}" alt="${producto.nombre}">
+//             </a>
+//         </body>
+//         </html>
+//     `;
+// }
 
 
 module.exports = {showProducts, showProductById, createProduct};
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -5,41 +5,14 @@ const {showProducts, showProductById,createProduct}=require('../controllers/prod
 
 const Producto=require('../models/Product.js');
 
-router.get('/products', async(req, res)=>{
-    try{
-        const html=await showProducts(1);
-        console.log(html);
-        res.status(200).send(html)
-    }
-    catch{
-        res.status(500).send('Error interno del servidor al crear el producto.');
-    }
-})
+router.get('/products', (req , res)=>{showProducts(req,res)});
 
-router.get('/products/:id', async(req, res)=>{
-    try{
-        const html=await showProductById(req.params.id);
-        console.log(html);
-        res.status(200).send(html)
-    }
-    catch{
-        res.status(500).send('Error interno del servidor al crear el producto.');
-    }
-})
+router.get('/products/:id', (req , res)=>{showProductById(req,res)});
 
-router.get('/dashboard', async(req, res)=>{
-    try{
-        const html=await showProduct(2);
-        console.log(html);
-        res.status(200).send(html)
-    }
-    catch{
-        res.status(500).send('Error interno del servidor al crear el producto.');
-    }
-})
+router.get('/dashboard', (req , res)=>{showProducts(req,res)});
 
 router.get('/dashboard/new', (req, res)=>{
-    res.render('new');
+    res.render('new',{title:'Mi proyecto'});
 })
 
 router.get('/dashboard/:id', async(req, res)=>{
@@ -63,68 +36,5 @@ router.post('/dashboard', async(req, res)=>{
         res.status(500).send('Error interno del servidor al crear el producto.');
     }
 })
-
-
-
-
-
-// router.get('/products', async (req, res)=>{
-//     try{
-//         const productos=await Producto.find();
-//         if(productos.length===0){
-//             res.status(401).send('No hay productos en Stock');
-//         }
-//         else{
-//             const productos=await Producto.find();
-//             const html_arr=[];
-//             productos.forEach(producto=>html_arr.push(generarProductoHTML(producto)))
-//             const html=html_arr.join('')
-//             console.log(html)
-//             res.status(200).send(html);
-//         }
-//     }
-//     catch{
-//         res.status(401).send('Error al leer la base de datos');
-//     }
-// })
-
-//Utilizaremos el ID que asigna la base de datos como el tipico nº de referencia que tienen los productos
-// router.get('/products/:productId', async(req, res)=>{
-//     try{
-//         const {productId}=req.params;
-//         const producto=await Producto.findById(productId);
-//         const html=generarProductoHTML(producto);
-//         res.status(200).send(html);
-//     }
-//     catch{
-//         res.status(400).send('Error: No hay prenda con este numero de referencia');
-//     }
-// })
-
-
-
-// router.put('/update/:id', async(req, res)=>{
-//     try{
-//         if(Object.keys(req.body).length===0){
-//             res.status(400).send('Error al actualizar el objeto: Esta vacío');
-//         }
-//         const {id}=req.params;
-//         const producto=await Producto.findByIdAndUpdate(id, req.body);
-//         res.send(producto);
-//     }
-//     catch{
-//         res.status(400).send('Error: No se ha podido actualizar la prenda');
-//     }
-// })
-
-// router.delete('/delete', async(req, res)=>{
-//     try{
-//         await Producto.deleteMany()
-//         res.status(200).send('Todas las prendas se ha borrado.')
-//     }
-//     catch{
-//         res.status(400).send('Error: No se ha podido borra la base de datos');
-//     }
-// })
 
 module.exports={router};
