1856721354b527f7872cb670fd523f6d87a09646
Pablo Portilla Rivero <pportilla@rfe.es>
2024-06-10 09:39:20 +0200

    Ejercicio actualizado los endpoints que tenia con la renderización con pug y toda la funcionalidad en el controller

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,10 +1,11 @@
 const Producto=require('../models/Product.js');
 
-async function createProduct(producto){
+async function createProduct(req, res){
     try{
+        const producto = req.body;
         const producto_mongoose=new Producto(producto);
         await producto_mongoose.save();
-        return producto_mongoose;
+        res.redirect(`/dashboard/${producto_mongoose._id}`);
     }
     catch (error) {
         console.error('Error al guardar el producto:', error);
@@ -36,41 +37,6 @@ async function showProductById(req ,res){
         throw new Error('Error al mostrar el producto');
     }
 }
-
-// function generarProductoHTML(producto) {
-    
-//     return `
-//         <html>
-//         <body>
-//             <p><strong>Referencia:</strong> ${producto._id}</p>
-//             <p><strong>Descripción:</strong> ${producto.descripcion}</p>
-//             <p><strong>Categoría:</strong> ${producto.categoria}</p>
-//             <p><strong>Precio:</strong> ${producto.precio}</p>
-//             <p><strong>Tamaño:</strong> ${producto.talla}</p>
-//             <img src="${producto.imagen}" alt="${producto.nombre}">
-
-//         </body>
-//         </html>
-//     `;
-// }
-
-
-// function generarProductoHTMLConEnlace(producto) {
-//     return `
-//         <html>
-//         <body>
-//             <p><strong>Referencia:</strong> ${producto._id}</p>
-//             <p><strong>Descripción:</strong> ${producto.descripcion}</p>
-//             <p><strong>Categoría:</strong> ${producto.categoria}</p>
-//             <p><strong>Precio:</strong> ${producto.precio}</p>
-//             <p><strong>Tamaño:</strong> ${producto.talla}</p>
-//             <a href="/products/${producto._id}">
-//                 <img src="${producto.imagen}" alt="${producto.nombre}">
-//             </a>
-//         </body>
-//         </html>
-//     `;
-// }
 
 
 module.exports = {showProducts, showProductById, createProduct};
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -15,26 +15,8 @@ router.get('/dashboard/new', (req, res)=>{
     res.render('new',{title:'Mi proyecto'});
 })
 
-router.get('/dashboard/:id', async(req, res)=>{
-    try{
-        const html=await showProductById(req.params.id);
-        console.log(html);
-        res.status(200).send(html)
-    }
-    catch{
-        res.status(500).send('Error interno del servidor al crear el producto.');
-    }
-})
+router.get('/dashboard/:id', (req , res)=>{showProductById(req,res)});
 
-router.post('/dashboard', async(req, res)=>{
-    const producto_obj = req.body;
-    try {
-        const obj = await createProduct(producto_obj);
-        console.log(obj);
-        res.redirect(303, `/dashboard/${obj._id}`);
-    } catch (error) {
-        res.status(500).send('Error interno del servidor al crear el producto.');
-    }
-})
+router.post('/dashboard', (req , res)=>{createProduct(req,res)});
 
 module.exports={router};
